761f74db5e6e512f52724e81479a3965335a7ec6
QwertygidQ <Qwertygid@gmail.com>
2018-11-30 22:01:32 +0300

    Kinda animated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import './css/Page.css';
+import './css/Animations.css';
 
 import Logo from './Logo';
 import UploadCard from './UploadCard';
@@ -16,8 +18,8 @@ class App extends React.Component {
 
     componentDidMount() {
         this.setState({
-            current_state: "Upload",
-            transition_stage: "state-enter",
+            current_state: 'Upload',
+            transition_stage: 'state-enter'
         });
     }
 
@@ -25,7 +27,19 @@ class App extends React.Component {
         return (
             <div className="page">
                     <Logo />
-                    <UploadCard />
+                    <CSSTransition
+                        in={ this.state.transition_stage === "state-enter" }
+                        classNames="statechange"
+                        timeout={ 1000 }
+                        onExited={ () => {  // state change from "state-enter" to "state-exit"
+                            this.setState({
+                                current_state: this.state.next_state,
+                                transition_stage: "state-enter" 
+                            });
+                        } }
+                        unmountOnExit>
+                        <UploadCard />
+                    </CSSTransition>
             </div>
         );
     }
--- src/Logo.js
+++ src/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './css/Logo.css';
+import './css/Animations.css';
 
 class Logo extends React.Component {
     render() {
         return (
-            <div className="text-center">
-                <h1 className="logo">SENIC</h1>
+            <div className="logo-enter text-center">
+                <h1 className="animated fadeIn logo">SENIC</h1>
             </div>
         );
     }
