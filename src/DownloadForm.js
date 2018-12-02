56d6b9f8ebb16a1a182848df1337433a3f8e53f8
anon <anon@example.com>
2018-12-02 12:19:15 +0300

    hack

--- src/DownloadForm.js
+++ src/DownloadForm.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import './css/Picture.css';
 
-const SERVER_ADDR = 'http://61ce14a8.ngrok.io';
+const SERVER_ADDR = 'https://f8952d97.ngrok.io';
 
 class DownloadForm extends React.Component {
     constructor(props) {
--- src/UploadForm.js
+++ src/UploadForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-const SERVER_ADDR = 'http://61ce14a8.ngrok.io'
+const SERVER_ADDR = 'https://f8952d97.ngrok.io'
 
 class UploadForm extends React.Component {
     constructor(props) {
